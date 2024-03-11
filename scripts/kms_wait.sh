2932806d7686523de693a42293c7fb8bcbc371ff
Takuro Sato <takurosato@microsoft.com>
2024-03-11 13:45:01 +0000

    Allow to configure extra sandbox args

--- scripts/kms_wait.sh
+++ scripts/kms_wait.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+KMS_URL="${KMS_URL:-https://127.0.0.1:8000}"
 response_code=000
 echo "Waiting for KMS to start..."
 while ! [[ $response_code -eq 400 || $response_code -eq 200 ]]; do
     sleep 1
-    response_code=$(curl https://127.0.0.1:8000/app/listpubkeys -k -s -o /dev/null -w "%{http_code}")
+    response_code=$(curl $KMS_URL/app/listpubkeys -k -s -o /dev/null -w "%{http_code}")
 done
 echo "KMS started"
