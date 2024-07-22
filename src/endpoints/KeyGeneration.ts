5eaf0b8a1647cadb0f690c18f5cab03d3046c600
beejones <ronny.bjones@hotmail.com>
2024-07-22 07:14:02 +0000

    incremental cleanup IKeyItems, KeyGeneration

--- src/endpoints/KeyGeneration.ts
+++ src/endpoints/KeyGeneration.ts
@@ -5,8 +5,11 @@ import * as ccfcrypto from "@microsoft/ccf-app/crypto";
 import { ccf } from "@microsoft/ccf-app/global";
 import { Base64 } from "js-base64";
 import { IKeyItem } from "./IKeyItem";
-import { arrayBufferToHex } from "../utils/Tooling";
+import { arrayBufferToHex, enableEndpoint } from "../utils/Tooling";
 import { Logger } from "../utils/Logger";
+
+// Enable the endpoint
+enableEndpoint();
 
 export class KeyGeneration {
   // Calculate a sha2 digest
