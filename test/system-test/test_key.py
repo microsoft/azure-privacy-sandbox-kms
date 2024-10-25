f6b0fe05e038f6d630681ff9050a6c1c2ce72431
Dominic Ayre <dominicayre@microsoft.com>
2024-10-25 15:55:03 +0000

    Improve key tests

--- test/system-test/test_key.py
+++ test/system-test/test_key.py
@@ -31,7 +31,6 @@ def test_no_key_release_policy(setup_kms):
     assert status_code == 500
 
 
-@pytest.mark.xfail(strict=True)
 def test_with_keys_and_policy(setup_kms):
     apply_kms_constitution(setup_kms["url"])
     apply_key_release_policy(setup_kms["url"])
@@ -45,7 +44,10 @@ def test_with_keys_and_policy(setup_kms):
         if status_code != 202:
             break
     assert status_code == 200
-    assert key_json["wrapped"] != ""
+
+    # Key isn't actually returned here, just the id of a key
+    assert key_json["wrappedKid"] != ""
+    assert key_json["wrapped"] == ""
 
 
 def test_key_with_multiple(setup_kms):
@@ -71,7 +73,26 @@ def test_key_with_multiple(setup_kms):
 # def test_key_incorrectly_signed_attestation(setup_kms): ...
 
 
-# def test_key_invalid_wrapping_key(setup_kms): ...
+def test_key_invalid_wrapping_key(setup_kms):
+    # Because privacy sandbox has a two endpoint scheme to return a wrapped key
+    # then unwrap it but we don't need it, this endpoint doesn't care about the
+    # wrapping key because the private key isn't wrapped.
+    apply_kms_constitution(setup_kms["url"])
+    apply_key_release_policy(setup_kms["url"])
+    refresh(setup_kms["url"])
+    while True:
+        status_code, key_json = key(
+            kms_url=setup_kms["url"],
+            attestation=setup_kms["attestation"],
+            wrapping_key='"invalidwrappingkey"',
+        )
+        if status_code != 202:
+            break
+    assert status_code == 200
+
+    # Key isn't actually returned here, just the id of a key
+    assert key_json["wrappedKid"] != ""
+    assert key_json["wrapped"] == ""
 
 
 # Test kid parameter
@@ -144,10 +165,9 @@ def test_key_fmt_tink(setup_kms):
         if status_code != 202:
             break
     assert status_code == 200
-    assert key_json["wrapped"] != ""
+    assert key_json["wrappedKid"] != ""
 
 
-@pytest.mark.xfail(strict=True)
 def test_key_fmt_jwk(setup_kms):
     apply_kms_constitution(setup_kms["url"])
     apply_key_release_policy(setup_kms["url"])
@@ -162,7 +182,10 @@ def test_key_fmt_jwk(setup_kms):
         if status_code != 202:
             break
     assert status_code == 200
-    assert key_json["wrapped"] != ""
+
+    # Key isn't actually returned here, just the id of a key
+    assert key_json["wrappedKid"] != ""
+    assert key_json["wrapped"] == ""
 
 
 def test_key_fmt_invalid(setup_kms):
