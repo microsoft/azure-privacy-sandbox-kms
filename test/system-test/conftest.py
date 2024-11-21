bb932658c33a9853983bad35c2c8a43786a81da9
Dominic Ayre <dominicayre@microsoft.com>
2024-11-21 08:48:07 +0000

    Print env vars when setting up

--- test/system-test/conftest.py
+++ test/system-test/conftest.py
@@ -7,16 +7,20 @@ from utils import deploy_app_code
 REPO_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", ".."))
 TEST_ENVIRONMENT = os.getenv("TEST_ENVIRONMENT", "ccf/sandbox-local")
 
+
 @pytest.fixture()
 def setup_kms():
 
     # Setup the CCF backend and set the environment accordingly
-    setup_vars = json.loads(subprocess.run(
-        f"scripts/{TEST_ENVIRONMENT}/up.sh",
-        cwd=REPO_ROOT,
-        check=True,
-        stdout=subprocess.PIPE,
-    ).stdout.decode())
+    setup_vars = json.loads(
+        subprocess.run(
+            f"scripts/{TEST_ENVIRONMENT}/up.sh",
+            cwd=REPO_ROOT,
+            check=True,
+            stdout=subprocess.PIPE,
+        ).stdout.decode()
+    )
+    print(setup_vars)
     os.environ.update(setup_vars)
 
     subprocess.run(
