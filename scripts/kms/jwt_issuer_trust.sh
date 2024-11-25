04159d1e70c0e84583d842f37d69402e1f5e4823
Dominic Ayre <dominicayre@microsoft.com>
2024-11-25 14:56:02 +0000

    Fix naming of jobs
    .
    
    TMP: Use permitted sub
    
    .
    
    .
    
    .
    
    Avoid races setting up ccf providers
    
    .
    
    .
    
    .
    
    Fix permissions on setup.sh
    
    Pre-start jwt issuer to prevent races
    
    Run tests sequentially
    
    Fix JWT test

--- scripts/kms/jwt_issuer_trust.sh
+++ scripts/kms/jwt_issuer_trust.sh
@@ -9,8 +9,8 @@ jwt-issuer-trust() {
   REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
 
   # Populate the JWK of the token issuer
-  PRIVATE_PEM="$WORKSPACE/private.pem"
-  CERT_PEM="$WORKSPACE/cert.pem"
+  PRIVATE_PEM="$JWT_ISSUER_WORKSPACE/private.pem"
+  CERT_PEM="$JWT_ISSUER_WORKSPACE/cert.pem"
   export JWK=$(npx pem-jwk "$PRIVATE_PEM" | jq --arg cert "$(cat "$CERT_PEM")" \
     '{kty, n, e} + {x5c: [$cert]} + {kid: "Demo IDP kid"}')
   envsubst < $REPO_ROOT/governance/proposals/set_jwt_issuer.json | jq > $WORKSPACE/proposals/set_jwt_issuer.json
--- test/system-test/conftest.py
+++ test/system-test/conftest.py
@@ -5,40 +5,62 @@ import pytest
 from utils import deploy_app_code
 import string
 import random
+import uuid
+import time
+import hashlib
+import base64
 
 REPO_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", ".."))
 TEST_ENVIRONMENT = os.getenv("TEST_ENVIRONMENT", "ccf/sandbox_local")
 
-
 @pytest.fixture()
 def setup_kms():
 
-    os.environ["DEPLOYMENT_NAME"] = os.getenv(
+    deployment_name = os.getenv(
         "DEPLOYMENT_NAME",
         f"kms-{''.join(random.choices(string.ascii_letters + string.digits, k=8))}".lower()
     )
+    os.environ["DEPLOYMENT_NAME"] = deployment_name
 
     # Setup the CCF backend and set the environment accordingly
     res = subprocess.run(
-            [f"scripts/{TEST_ENVIRONMENT}/up.sh", "--force-recreate"],
+        [f"scripts/{TEST_ENVIRONMENT}/up.sh", "--force-recreate"],
+        cwd=REPO_ROOT,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    try:
+        if res.returncode != 0:
+            raise Exception(f"HERE: {res.stdout.decode()} - {res.stderr.decode()}")
+
+        stdout = res.stdout.decode()
+        setup_vars = json.loads(stdout[stdout.rfind("{"):])
+        os.environ.update(setup_vars)
+
+        res = subprocess.run(
+            ["make", "jwt-issuer-up"],
             cwd=REPO_ROOT,
             check=True,
             stdout=subprocess.PIPE,
-        ).stdout.decode()
-    setup_vars = json.loads(res[res.rfind("{"):])
-    os.environ.update(setup_vars)
+            stderr=subprocess.PIPE,
+        )
+        stdout = res.stdout.decode()
+        setup_vars = json.loads(stdout[stdout.rfind("{"):])
+        os.environ.update(setup_vars)
 
-    subprocess.run(
-        ["make", "jwt-issuer-up"],
-        cwd=REPO_ROOT,
-        check=True,
-    )
-    os.environ["JWT_ISSUER"] = "http://localhost:3000/token"
+        deploy_app_code()
 
-    deploy_app_code()
+        yield
 
-    yield
+    except Exception as e:
+        print(e)
+        try:
+            print(res.stdout.decode())
+            print(res.stderr.decode())
+        except Exception: ...
+        raise
 
-    subprocess.run(
-        f"scripts/{TEST_ENVIRONMENT}/down.sh",
-    )
+    finally:
+        subprocess.run(
+            [f"scripts/{TEST_ENVIRONMENT}/down.sh", deployment_name],
+        )
