ada4db931270f1eb4fd96f8a8b1d3318d02ba8a5
Dominic Ayre <dominicayre@microsoft.com>
2024-11-29 09:40:09 +0000

    Use latest cleanroom extension

--- scripts/ccf/az-cleanroom-aci/setup.sh
+++ scripts/ccf/az-cleanroom-aci/setup.sh
@@ -4,8 +4,17 @@
 # Licensed under the MIT license.
 
 az-cleanroom-aci-setup() {
-    az extension add -y --allow-preview true \
-    --source https://cleanroomazcli.blob.core.windows.net/azcli/cleanroom-0.0.7-py2.py3-none-any.whl
+    az storage blob download \
+        --account-name azurekmsstorage \
+        --container-name azure-cleanroom \
+        --name cleanroom-1.0.0-py2.py3-none-any.whl \
+        --auth-mode login \
+        --file /tmp/cleanroom-1.0.0-py2.py3-none-any.whl > /dev/null
+
+    az extension add -y --allow-preview true --upgrade \
+        --source /tmp/cleanroom-1.0.0-py2.py3-none-any.whl
+
+    rm /tmp/cleanroom-1.0.0-py2.py3-none-any.whl
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
