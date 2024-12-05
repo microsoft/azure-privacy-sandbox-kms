2e5353c2c44506bc63a6cc549629b1a2152b33c6
Dominic Ayre <dominicayre@microsoft.com>
2024-12-05 13:12:32 +0000

    Refactor signing CCF proposals into standalone function

---
+++ scripts/ccf/sign.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+ccf-sign() {
+
+    content=$1
+    msg_type=${2:-"proposal"}
+
+    if [[ $USE_AKV == false ]]; then
+        ccf_cose_sign1 \
+            --content $content \
+            --signing-cert ${KMS_MEMBER_CERT_PATH} \
+            --signing-key ${KMS_MEMBER_PRIVK_PATH} \
+            --ccf-gov-msg-type $msg_type \
+            --ccf-gov-msg-created_at $(date -Is)
+    else
+        creation_time=$(date -u +"%Y-%m-%dT%H:%M:%S")
+        bearer_token=$( \
+            az account get-access-token \
+            --resource https://vault.azure.net \
+            --query accessToken --output tsv \
+        )
+        signature=$(mktemp)
+        ccf_cose_sign1_prepare \
+            --ccf-gov-msg-type $msg_type \
+            --ccf-gov-msg-created_at $creation_time \
+            --content $content \
+            --signing-cert ${KMS_MEMBER_CERT_PATH} | \
+            curl -X POST -s \
+                -H "Authorization: Bearer $bearer_token" \
+                -H "Content-Type: application/json" \
+                "${AKV_URL}/keys/${AKV_KEY_NAME}/sign?api-version=7.2" \
+                -d @- > $signature
+        ccf_cose_sign1_finish \
+            --ccf-gov-msg-type $msg_type \
+            --ccf-gov-msg-created_at $creation_time \
+            --content $content \
+            --signing-cert ${KMS_MEMBER_CERT_PATH} \
+            --signature $signature
+        rm -rf $signature
+    fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    ccf-sign "$@"
+fi
