f14d0f13b9501069be5c9d246e6d3fbfd5af3125
Dominic Ayre <dominicayre@microsoft.com>
2024-12-17 14:33:51 +0000

    Fix typo

--- scripts/jwt_issuer/up.sh
+++ scripts/jwt_issuer/up.sh
@@ -10,7 +10,7 @@ jwt-issuer-up() {
     export JWT_ISSUER_WORKSPACE=$(realpath ${JWT_ISSUER_WORKSPACE:-$REPO_ROOT/jwt_issuer_workspace_${UNIQUE_ID:-services}})
     mkdir -p $JWT_ISSUER_WORKSPACE
 
-    docker compose -p ${UNIQUE_ID-:services} -f services/docker-compose.yml up jwt-issuer --wait "$@"
+    docker compose -p ${UNIQUE_ID:-services} -f services/docker-compose.yml up jwt-issuer --wait "$@"
     sudo chown $USER:$USER -R $JWT_ISSUER_WORKSPACE
 
     export JWT_ISSUER="$(cat $JWT_ISSUER_WORKSPACE/jwt_issuer_address)/token"
