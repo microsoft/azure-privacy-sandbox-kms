d3a28d19759471b838d519edf293d2a3ccb042fe
Dominic Ayre <dominicayre@microsoft.com>
2024-12-18 16:16:31 +0000

    Fix missing JWT_ISSUER_WORKSPACE

--- scripts/jwt_issuer/up.sh
+++ scripts/jwt_issuer/up.sh
@@ -7,7 +7,7 @@ jwt-issuer-up() {
     set -e
 
     REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
-    export JWT_ISSUER_WORKSPACE=$(realpath ${JWT_ISSUER_WORKSPACE:-$REPO_ROOT/jwt_issuers_workspace/${UNIQUE_ID:-default}/})
+    export JWT_ISSUER_WORKSPACE=${JWT_ISSUER_WORKSPACE:-$REPO_ROOT/jwt_issuers_workspace/${UNIQUE_ID:-default}/}
     mkdir -p $JWT_ISSUER_WORKSPACE
 
     docker compose -p ${UNIQUE_ID:-default} -f services/docker-compose.yml up jwt-issuer --wait "$@"
