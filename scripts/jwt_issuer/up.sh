063a89b61c56f39d98db4016a52d6bfb1d090865
Dominic Ayre <dominicayre@microsoft.com>
2024-12-18 11:58:57 +0000

    Scope JWT issuer workspaces in a single dir

--- scripts/jwt_issuer/up.sh
+++ scripts/jwt_issuer/up.sh
@@ -7,10 +7,10 @@ jwt-issuer-up() {
     set -e
 
     REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
-    export JWT_ISSUER_WORKSPACE=$(realpath ${JWT_ISSUER_WORKSPACE:-$REPO_ROOT/jwt_issuer_workspace_${UNIQUE_ID:-services}})
+    export JWT_ISSUER_WORKSPACE=$(realpath ${JWT_ISSUER_WORKSPACE:-$REPO_ROOT/jwt_issuers_workspace/${UNIQUE_ID:-default}/})
     mkdir -p $JWT_ISSUER_WORKSPACE
 
-    docker compose -p ${UNIQUE_ID:-services} -f services/docker-compose.yml up jwt-issuer --wait "$@"
+    docker compose -p ${UNIQUE_ID:-default} -f services/docker-compose.yml up jwt-issuer --wait "$@"
     sudo chown $USER:$USER -R $JWT_ISSUER_WORKSPACE
 
     export JWT_ISSUER="$(cat $JWT_ISSUER_WORKSPACE/jwt_issuer_address)/token"
