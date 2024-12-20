e1ee96085f988a01bf3454042e0272b99e6b5c73
Dominic Ayre <dominicayre@microsoft.com>
2024-12-20 16:18:14 +0000

    Remove session scope for JWT issuer

--- scripts/jwt_issuer/down.sh
+++ scripts/jwt_issuer/down.sh
@@ -7,5 +7,5 @@ set -e
 
 docker compose -p ${UNIQUE_ID:-default} -f services/docker-compose.yml down jwt-issuer --remove-orphans
 
+unset JWT_ISSUER_WORKSPACE
 unset JWT_ISSUER
-unset JWT_TOKEN_ISSUER_URL
