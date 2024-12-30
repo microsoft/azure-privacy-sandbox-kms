5b4372d3547f6d49c022bbbfa690af2b74b4e263
mohamedelb <mohamedelb@microsoft.com>
2024-12-30 10:30:24 +0000

    Add key deprecation logic

--- src/endpoints/keyEndpoint.ts
+++ src/endpoints/keyEndpoint.ts
@@ -11,11 +11,11 @@ import { IAttestationReport } from "../attestation/ISnpAttestationReport";
 import { IKeyItem } from "./IKeyItem";
 import { KeyGeneration } from "./KeyGeneration";
 import { validateAttestation } from "../attestation/AttestationValidation";
-import { hpkeKeyIdMap, hpkeKeysMap } from "../repositories/Maps";
+import { hpkeKeyIdMap, hpkeKeysMap, keyRotationPolicyMap } from "../repositories/Maps";
 import { ServiceRequest } from "../utils/ServiceRequest";
 import { LogContext, Logger } from "../utils/Logger";
-import { Settings } from "../policies/Settings";
 import { TrustedTime } from "../utils/TrustedTime";
+import { KeyRotationPolicy } from "../policies/KeyRotationPolicy";
 
 // Enable the endpoint
 enableEndpoint();
@@ -178,24 +178,28 @@ export const key = (
     );
   }
 
-  const settings = Settings.loadSettings().settings;
-  const gracePeriodDays = settings.keyRotation.gracePeriodDays;
-
+  const keyRotation = KeyRotationPolicy.loadKeyRotationPolicyFromMap(keyRotationPolicyMap, logContext).keyRotationPolicy;
+  const gracePeriodSeconds = keyRotation.gracePeriodSeconds;
+  const rotationIntervalSeconds = keyRotation.rotationIntervalSeconds;
   // Get the current time using TrustedTime
   const currentTime = TrustedTime.getCurrentTime();
   const currentDate = new Date(currentTime);
 
-  // Get the expiry date of the key
-  const expiryDate = new Date(keyItem.expiryDate!);
+  // Get the creation date of the key
+  const creationDate = new Date(keyItem.timestamp!);
 
+  // Calculate the expiry date of the key by adding the rotation interval to the creation date
+  const expiryDateMillis = creationDate.getTime() + rotationIntervalSeconds * 1000;
+  const expiryDate = new Date(expiryDateMillis);
   // Calculate the grace period start date by subtracting the grace period days from the expiry date
-  const gracePeriodMillis = gracePeriodDays * 24 * 60 * 60 * 1000;
+  const gracePeriodMillis = gracePeriodSeconds * 1000;
   const gracePeriodStartDate = new Date(expiryDate.getTime() - gracePeriodMillis);
 
   if (currentDate > expiryDate) {
     return ServiceResult.Failed<string>(
       { errorMessage: `${name}: Key has expired and is no longer valid` },
       400,
+      logContext
     );
   } else if (currentDate > gracePeriodStartDate) {
     Logger.warn(`${name}: Key is deprecated and will expire soon`);
