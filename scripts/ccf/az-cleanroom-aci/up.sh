7ffc4fd15dcfa1d050b185aa73d3385dc454cf6f
Dominic Ayre <dominicayre@microsoft.com>
2025-01-06 15:14:09 +0000

    Fix retry mechanism in az cleanroom CCF network up (#275)
    * Fix retry mechanism in CCF network up
    
    * Run CI on PRs targeting any branch
    
    * Also check for az cli command failure

--- scripts/ccf/az-cleanroom-aci/up.sh
+++ scripts/ccf/az-cleanroom-aci/up.sh
@@ -27,18 +27,28 @@ az-cleanroom-aci-up() {
 
     retries=10
     ccf_up=false
-    while [ $ccf_up = false ] && [ $retries -gt 0 ]; do
+    success=false
+    set +e
+    while [ $ccf_up = false ] && [ $success = false ] && [ $retries -gt 0 ]; do
         az cleanroom ccf network up \
             --subscription $SUBSCRIPTION \
             --resource-group $RESOURCE_GROUP \
             --provider-client "$DEPLOYMENT_NAME-provider" \
             --name $DEPLOYMENT_NAME
+        if [ $? -eq 0 ]; then
+            success=true
+        fi
         kms_url=`az-cleanroom-aci-get-url`
         if curl -k $kms_url/node/network -o /dev/null -s -w "%{http_code}" | grep -q 200; then
             ccf_up=true
         fi
         retries=$((retries - 1))
     done
+    set -e
+
+    if [ $success = false ] || [ $ccf_up = false ]; then
+        exit 1
+    fi
 
     export KMS_URL=`az-cleanroom-aci-get-url`
 
@@ -47,7 +57,7 @@ az-cleanroom-aci-up() {
     export KMS_MEMBER_PRIVK_PATH="$WORKSPACE/ccf-operator_privk.pem"
 
     mkdir -p $WORKSPACE/proposals
-    
+
     set +e
 }
 
