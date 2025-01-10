2809bbb0936ca82c4fa74cca193e3095bf7ea6dd
beejones <ronny.bjones@hotmail.com>
2025-01-10 15:23:38 +0000

    Address feedback

--- test/system-test/test_unwrapkey.py
+++ test/system-test/test_unwrapkey.py
@@ -3,6 +3,11 @@ import pytest
 from endpoints import key, refresh, unwrapKey
 from utils import apply_kms_constitution, apply_key_release_policy, trust_jwt_issuer, get_test_attestation, get_test_public_wrapping_key, decrypted_wrapped_key, apply_settings_policy
 
+# This test will check the two step google protocol to retrieve a private key
+# Step 1, call the /key endpoint and retrieve the kid
+# Step 2, call the /unwrapKey endpoint with the kid and the wrapping key to retrieve a wrapped private key
+# Step 3, decrypt the wrapped private key
+
 
 def test_unwrap_key_and_decrypt(setup_kms):
     apply_kms_constitution()
@@ -82,20 +87,25 @@ def test_unwrap_key_missing_wrappedKid(setup_kms):
     apply_key_release_policy()
     apply_settings_policy()
     refresh()
-    while True:
-        status_code, key_json = key(
-            attestation=get_test_attestation(),
-            wrapping_key=get_test_public_wrapping_key(),
-        )
-        if status_code != 202:
-            break
-    assert status_code == 200
 
     # unwrap key
     status_code, unwrapped_json = unwrapKey(
         attestation=get_test_attestation(),
         wrapping_key=get_test_public_wrapping_key(),
         wrappedKid=""
+    )
+    assert status_code == 404
+
+
+def test_unwrap_key_without_refresh(setup_kms):
+    apply_kms_constitution()
+    apply_key_release_policy()
+
+    # unwrap key
+    status_code, unwrapped_json = unwrapKey(
+        attestation=get_test_attestation(),
+        wrapping_key=get_test_public_wrapping_key(),
+        wrappedKid="abcd"
     )
     assert status_code == 404
 
