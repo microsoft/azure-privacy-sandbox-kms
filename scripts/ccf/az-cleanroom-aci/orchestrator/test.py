15e5cfca58ee068403811dd12f56dd577680adb4
Dominic Ayre <dominicayre@microsoft.com>
2025-01-10 15:41:57 +0000

    Add better logging

--- scripts/ccf/az-cleanroom-aci/orchestrator/test.py
+++ scripts/ccf/az-cleanroom-aci/orchestrator/test.py
@@ -8,14 +8,17 @@ def test_kill_node(az_cleanroom):
     result = nodes_scale(node_count, get_logs=True)
     assert len(result["nodes"]) == node_count
 
+    print("3 node network running, killing one node through ACI")
     stop_node(node_url_to_name(result['nodes'][-1]))
 
-    # Confirm that the node is dead
+    print("Waiting for the network to see node as unhealthy")
     wait_for_network_condition(
         condition=lambda nh: healthy_node_count(nh) == node_count - 1,
     )
+    print("Unhealthy node found")
 
-    # Wait for the orchestrator to heal the network
+    print("Waiting for network to heal")
+    print("  i.e. 3 available nodes and no nodes requiring replacement")
     wait_for_network_condition(
         condition=lambda nh: all([
             healthy_node_count(nh) == node_count,
