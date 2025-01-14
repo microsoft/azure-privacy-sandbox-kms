dc4a4630117dbbce6e224a5090e5fbd527d76c61
Takuro Sato <takurosato@microsoft.com>
2025-01-14 10:38:22 +0000

    Print debug etc

--- scripts/tools/install-deps.sh
+++ scripts/tools/install-deps.sh
@@ -6,6 +6,9 @@ REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
 
 set -x
 
-pip install -q -r $REPO_ROOT/requirements.txt
-npm install --silent --prefix $REPO_ROOT/
+date
+pip install -q -r $REPO_ROOT/requirements.txt -vvv
+date
+npm install --silent --prefix $REPO_ROOT/ --dd
+date
 $REPO_ROOT/scripts/tools/install-c-aci-testing.sh > /dev/null
