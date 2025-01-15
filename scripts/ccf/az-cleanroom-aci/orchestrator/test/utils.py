d576653abdf0083c3f5e68aae135838a6d937a1e
Dominic Ayre <dominicayre@microsoft.com>
2025-01-15 13:36:54 +0000

    Wait for orchestrator to be healthy

--- scripts/ccf/az-cleanroom-aci/orchestrator/test/utils.py
+++ scripts/ccf/az-cleanroom-aci/orchestrator/test/utils.py
@@ -165,7 +165,7 @@ def deploy_orchestrator():
         [
             "docker", "compose",
             "-f", f"{REPO_ROOT}/scripts/ccf/az-cleanroom-aci/orchestrator/compose.yml",
-            "up", "ccf-orchestrator", "-d", "--build"
+            "up", "ccf-orchestrator", "--wait", "--build"
         ],
         check=True,
     )
