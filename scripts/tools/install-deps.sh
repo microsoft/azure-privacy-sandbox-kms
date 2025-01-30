07df9742c62fe83d10e2df4f9972d21bc875202c
beejones <ronny.bjones@hotmail.com>
2025-01-30 09:34:11 +0000

    fix priviledge issue

--- scripts/tools/install-deps.sh
+++ scripts/tools/install-deps.sh
@@ -6,7 +6,7 @@ REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
 
 set -x
 
-$REPO_ROOT/.devcontainer/install_packages.sh > /dev/null
+sudo $REPO_ROOT/.devcontainer/install_packages.sh > /dev/null
 pip install -q -r $REPO_ROOT/requirements.txt
 npm install --silent --prefix $REPO_ROOT/
 $REPO_ROOT/scripts/tools/install-c-aci-testing.sh > /dev/null
