81e2c9175f162e05c9097b94a14ab2a85ae09f4b
Dominic Ayre <dominicayre@microsoft.com>
2025-02-13 13:15:47 +0000

    Support bringing up an ACL based KMS

---
+++ scripts/ccf/acl/up.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../..")"
+
+acl-up() {
+
+    source $REPO_ROOT/services/cacitesting.env
+
+    DEPLOYMENT_NAME=${DEPLOYMENT_NAME:-$1}
+    if [ -z "$DEPLOYMENT_NAME" ]; then
+        read -p "Enter deployment name: " DEPLOYMENT_NAME
+    fi
+    export DEPLOYMENT_NAME
+
+    # Deploy the confidential ledger
+    az confidentialledger create \
+        --name $DEPLOYMENT_NAME \
+        --subscription $SUBSCRIPTION \
+        --resource-group $RESOURCE_GROUP \
+        --location "AustraliaEast" \
+        --ledger-type "Public" \
+        --aad-based-security-principals ledger-role-name="Administrator" principal-id="$(az account show | jq -r '.id')"
+    export KMS_URL="https://$DEPLOYMENT_NAME.confidential-ledger.azure.com"
+
+    # Save the service certificate
+    export WORKSPACE=~/$DEPLOYMENT_NAME.aclworkspace
+    mkdir -p $WORKSPACE
+    curl https://identity.confidential-ledger.core.azure.com/ledgerIdentity/$DEPLOYMENT_NAME \
+        | jq -r '.ledgerTlsCertificate' > $WORKSPACE/service_cert.pem
+    export KMS_SERVICE_CERT_PATH="$WORKSPACE/service_cert.pem"
+
+    # For ACL we don't have members
+    export KMS_MEMBER_CERT_PATH=""
+    export KMS_MEMBER_PRIVK_PATH=""
+}
+
+acl-up "$@"
+
+jq -n '{
+    DEPLOYMENT_NAME: env.DEPLOYMENT_NAME,
+    WORKSPACE: env.WORKSPACE,
+    KMS_URL: env.KMS_URL,
+    KMS_SERVICE_CERT_PATH: env.KMS_SERVICE_CERT_PATH,
+    KMS_MEMBER_CERT_PATH: env.KMS_MEMBER_CERT_PATH,
+    KMS_MEMBER_PRIVK_PATH: env.KMS_MEMBER_PRIVK_PATH
+}'
