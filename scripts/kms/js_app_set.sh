dc915e41bb58704ac3be52e2cc1c168018d3da88
Dominic Ayre <dominicayre@microsoft.com>
2025-02-21 09:15:22 +0000

    Support bringing up an ACL based KMS (#321)

--- scripts/kms/js_app_set.sh
+++ scripts/kms/js_app_set.sh
@@ -3,13 +3,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-js-app-set() {
-  set -e
-
-  REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
-
-  # Build the KMS bundle
-  npm install && npm run build
+propose_set_js_app() {
+  # For plain CCF, application code is set via a governance proposal
 
   # Adapt template line by line since bundle.json is too big for sed or awk
   temp_file=$(mktemp)
@@ -27,6 +22,39 @@ js-app-set() {
   # Submit the proposal
   source $REPO_ROOT/scripts/ccf/propose.sh
   ccf-propose $WORKSPACE/proposals/set_js_app.json
+}
+
+
+call_user_defined_endpoints() {
+  # For ACL based applications, application code is set by calling an endpoint
+
+  # TODO: Flesh out a proper auth implementation, for now:
+  # Get the current users bearer token
+  export TOKEN=$(az account get-access-token --resource https://confidential-ledger.azure.com | jq -r '.accessToken')
+
+  curl -k \
+    -X PUT -H "Authorization: Bearer $TOKEN" \
+    -H "Content-Type: application/json" \
+    -d @$REPO_ROOT/dist/bundle.json \
+    "$KMS_URL/app/userDefinedEndpoints?api-version=2024-08-22-preview"
+
+  echo "userDefinedEndpoints set"
+}
+
+
+js-app-set() {
+  set -e
+
+  REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
+
+  # Build the KMS bundle
+  npm install && npm run build
+
+  if [[ "$KMS_URL" == *"confidential-ledger.azure.com" ]]; then
+    call_user_defined_endpoints
+  else
+    propose_set_js_app
+  fi
 
   set +e
 }
