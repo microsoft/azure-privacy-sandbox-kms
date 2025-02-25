a25aaf70d400aafe7b558202aabfb0c86f912ef0
Dominic Ayre <dominicayre@microsoft.com>
2025-02-25 14:30:05 +0000

    .

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -2,9 +2,7 @@
 // Licensed under the MIT license.
 
 import { Action } from "./default_ccf";
-import { action as settingsPolicyAction } from "./set_settings_policy";
 
 
 export const actions = new Map<string, Action>([
-    settingsPolicyAction,
 ]);
