7b2d6d9d76131e0cc3fc2f9bcd3990efd747a694
Dominic Ayre <dominicayre@microsoft.com>
2025-02-28 14:19:58 +0000

    Split set_jwt_issuer and set_jwt_validation_policy in jwt_issuer_trust

--- scripts/kms/jwt_issuer_trust.sh
+++ scripts/kms/jwt_issuer_trust.sh
@@ -3,21 +3,76 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-jwt-issuer-trust() {
+REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
+
+set_jwt_issuer() {
   set -e
 
-  REPO_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")"
-
-  # Populate the JWK of the token issuer
-  PRIVATE_PEM="$JWT_ISSUER_WORKSPACE/private.pem"
-  CERT_PEM="$JWT_ISSUER_WORKSPACE/cert.pem"
-  export JWK=$(npx pem-jwk "$PRIVATE_PEM" | jq --arg cert "$(cat "$CERT_PEM")" \
-    '{kty, n, e} + {x5c: [$cert]} + {kid: "Demo IDP kid"}')
-  envsubst < $REPO_ROOT/governance/proposals/set_jwt_issuer.json | jq > $WORKSPACE/proposals/set_jwt_issuer.json
+  envsubst < $REPO_ROOT/governance/proposals/set_jwt_issuer.json \
+    | jq > $WORKSPACE/proposals/set_jwt_issuer.json
 
   # Submit the proposal
   source $REPO_ROOT/scripts/ccf/propose.sh
   ccf-propose $WORKSPACE/proposals/set_jwt_issuer.json
+
+  set +e
+}
+
+set_jwt_validation_policy() {
+  set -e
+
+  envsubst < $REPO_ROOT/governance/proposals/set_jwt_validation_policy.json \
+    | jq > $WORKSPACE/proposals/set_jwt_validation_policy.json
+
+  # Submit the proposal
+  source $REPO_ROOT/scripts/ccf/propose.sh
+  ccf-propose $WORKSPACE/proposals/set_jwt_validation_policy.json
+
+  set +e
+}
+
+use_demo_issuer() {
+  set -e
+
+  export ISSUER="http://Demo-jwt-issuer"
+  export JWKS=$(\
+    npx pem-jwk "$JWT_ISSUER_WORKSPACE/private.pem" \
+      | jq \
+        --arg cert "$(cat "$JWT_ISSUER_WORKSPACE/cert.pem")" \
+        '{keys: [({kty, n, e} + {x5c: [$cert]} + {kid: "Demo IDP kid"})]}' \
+          | sed -e '1s/^/"jwks": /' -e '$s/$/,/' \
+  )
+
+  set +e
+}
+
+jwt-issuer-trust() {
+  set -e
+
+  issuer="demo"
+
+  # Parse command-line arguments
+  while [[ $# -gt 0 ]]; do
+      case "$1" in
+          --demo)
+              issuer="demo"
+              shift 2
+              ;;
+          *)
+              echo "Unknown parameter: $1"
+              exit 1
+              ;;
+      esac
+  done
+
+  if [[ "$issuer" == "demo" ]]; then
+    use_demo_issuer
+  fi
+
+  set_jwt_issuer
+  set_jwt_validation_policy
+
+  set +e
 }
 
 jwt-issuer-trust "$@"
