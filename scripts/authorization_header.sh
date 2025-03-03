45b12bc94081031a5492b4002becee64bb52e8a1
Dominic Ayre <dominicayre@microsoft.com>
2025-03-03 16:33:37 +0000

    Fix auth header script

--- scripts/authorization_header.sh
+++ scripts/authorization_header.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # This script will generate an access token for the API and returns the authorization header.
 
 # Get JWT
-jwt_token=$(./scripts/generate_access_token.sh | jq -r '.access_token')
+jwt_token=$(. $JWT_ISSUER_WORKSPACE/fetch.sh && jwt_issuer_fetch)
 
 # Set the Authorization header content
 auth_header="Bearer $jwt_token"
