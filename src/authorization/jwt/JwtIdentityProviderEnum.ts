bd8b64edccf7322543a7487ba8ed29b149145f13
Dominic Ayre <dominicayre@microsoft.com>
2025-03-04 20:24:38 +0000

    Fix key of MS_AAD enum value

--- src/authorization/jwt/JwtIdentityProviderEnum.ts
+++ src/authorization/jwt/JwtIdentityProviderEnum.ts
@@ -5,6 +5,6 @@
  * JWT Identity Providers. List all supported IDPs
  */
 export enum JwtIdentityProviderEnum {
-  MS_AAD = "https://sts.windows.net/72f988bf-86f1-41af-91ab-2d7cd011db47/",
+  MS_AAD = "https://login.microsoftonline.com/common/v2.0",
   Demo = "http://Demo-jwt-issuer",
 }
