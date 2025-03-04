9dfd5a447732a733a8e3649372780af9db5a69ba
Dominic Ayre <dominicayre@microsoft.com>
2025-03-04 17:12:48 +0000

    Reduce the fields we check

--- scripts/kms/jwt_issuer_trust.sh
+++ scripts/kms/jwt_issuer_trust.sh
@@ -112,8 +112,7 @@ use_aad_issuer() {
   export ID_FIELDS=" \
     \"idtyp\": \"$(echo "$DECODED_JWT" | jq -r '.idtyp')\", \
     \"appid\": \"$(echo "$DECODED_JWT" | jq -r '.appid')\", \
-    \"appidacr\": \"$(echo "$DECODED_JWT" | jq -r '.appidacr')\", \
-    \"oid\": \"$(echo "$DECODED_JWT" | jq -r '.oid')\""
+    \"appidacr\": \"$(echo "$DECODED_JWT" | jq -r '.appidacr')\""
 
   set +e
 }
