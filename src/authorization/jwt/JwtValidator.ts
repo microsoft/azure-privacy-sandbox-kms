73b2ddafad2ee5d3785f206f363c961a5f62cec4
Dominic Ayre <dominicayre@microsoft.com>
2025-03-05 09:40:54 +0000

    Simplify checking JWT against policy

--- src/authorization/jwt/JwtValidator.ts
+++ src/authorization/jwt/JwtValidator.ts
@@ -6,6 +6,7 @@ import { ServiceResult } from "../../utils/ServiceResult";
 import { IValidatorService } from "../IValidationService";
 import { Logger, LogContext } from "../../utils/Logger";
 import { JwtValidationPolicyMap } from "./JwtValidationPolicyMap";
+import isMatch from 'lodash/isMatch';
 
 export class JwtValidator implements IValidatorService {
   private logContext: LogContext;
@@ -38,35 +39,15 @@ export class JwtValidator implements IValidatorService {
       `Validate JWT policy for issuer ${issuer}: ${JSON.stringify(policy)}`, logContext
     );
 
-    const keys = Object.keys(policy);
-
-    for (let inx = 0; inx < keys.length; inx++) {
-      const key = keys[inx];
-      const jwtProp = identity?.jwt?.payload[key];
-      let compliant = false;
-
-      // Check if policy[key] is an array
-      if (Array.isArray(policy[key])) {
-        // Check if jwtProp is in the array
-        compliant = policy[key].includes(jwtProp);
-      } else {
-        // Perform the existing equality check
-        compliant = jwtProp === policy[key];
-      }
-
-      Logger.debug(
-        `isValidJwtToken: ${key}, expected: ${policy[key]}, found: ${jwtProp}, ${compliant}`, logContext
+    // Check that the JWT payload is a matching superset of the policy
+    if (!isMatch(identity.jwt.payload, policy)) {
+      const errorMessage = `The JWT ${JSON.stringify(identity.jwt.payload)} doesn't match the policy ${JSON.stringify(policy)}`;
+      Logger.error(errorMessage, logContext);
+      return ServiceResult.Failed(
+        { errorMessage, errorType: "AuthenticationError" },
+        401,
+        logContext
       );
-
-      if (!compliant) {
-        const errorMessage = `The JWT has no valid ${key}, expected: ${policy[key]}, found: ${jwtProp}`;
-        Logger.error(errorMessage, logContext);
-        return ServiceResult.Failed(
-          { errorMessage, errorType: "AuthenticationError" },
-          401,
-          logContext
-        );
-      }
     }
 
     return ServiceResult.Succeeded("", logContext);
