9fb6f6d65e503839eb35c5711b915648a8d7c9dc
Dominic Ayre <dominicayre@microsoft.com>
2025-03-06 11:13:27 +0000

    Support user certs in auth endpoint script

--- scripts/kms/endpoints/auth.sh
+++ scripts/kms/endpoints/auth.sh
@@ -24,6 +24,8 @@ auth() {
     auth_arg=()
     if [[ "$auth" == "member_cert" ]]; then
         auth_arg=(--cert $KMS_MEMBER_CERT_PATH --key $KMS_MEMBER_PRIVK_PATH)
+    if [[ "$auth" == "user_cert" ]]; then
+        auth_arg=(--cert $KMS_USER_CERT_PATH --key $KMS_USER_PRIVK_PATH)
     elif [[ "$auth" == "jwt" ]]; then
         auth_arg=(-H "Authorization: Bearer $(curl -X POST $JWT_ISSUER | jq -r '.access_token')")
     fi
