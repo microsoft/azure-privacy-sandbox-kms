cdd6402d22df5fba3d76251a1e6dafbf86f84107
Dominic Ayre <dominicayre@microsoft.com>
2025-03-11 15:28:14 +0000

    Refactor running JWT issuer into function

--- docker/run_standalone_kms.sh
+++ docker/run_standalone_kms.sh
@@ -3,10 +3,23 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+run_jwt_issuer() {
+  (
+      cd test/utils/jwt
+      KMS_WORKSPACE=/kms/workspace npm run start 2>&1 &
+  )
+
+  export JWT_ISSUER_WORKSPACE=$WORKSPACE
+  export JWT_ISSUER="http://localhost:3000/token"
+
+  until curl -s -k -f -X POST $JWT_ISSUER > /dev/null 2>&1; do
+      sleep 1
+  done
+}
+
 mkdir -p workspace/proposals
 
-(cd test/utils/jwt && KMS_WORKSPACE=/kms/workspace nohup npm run start > nohup.out 2>&1 &)
-./scripts/wait_idp_ready.sh
+run_jwt_issuer
 
 JWK=$(npx pem-jwk "./workspace/private.pem" | \
       jq --arg cert "$(cat ./workspace/cert.pem)" '{kty, n, e} + {x5c: [$cert]} + {kid: "Demo IDP kid"}')
