ead34b2539d2f2ca7b1e0979fc849cf256e5ae24
Dominic Ayre <domayre@outlook.com>
2025-03-24 10:27:19 +0000

    Add documentation to sign.sh

--- scripts/ccf/sign.sh
+++ scripts/ccf/sign.sh
@@ -3,6 +3,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+# This script outputs a COSE Sign1 document, switching on the env variable
+# `USE_AKV`.
+#  - In the local key case, doing a simple one step signing.
+#  - In the AKV key case, using the two step process which prepares the document
+#    given the payload and signing cert, then uses AKV to sign the JSON output, and
+#    then finally calling finish to output the final COSE Sign1 document.
+
 ccf-sign() {
 
     content=$1
@@ -26,17 +33,17 @@ ccf-sign() {
             --query accessToken --output tsv \
         )
         signature=$(mktemp)
-        ccf_cose_sign1_prepare \
+        payload=$(ccf_cose_sign1_prepare \
             --ccf-gov-msg-type $msg_type \
             --ccf-gov-msg-created_at $creation_time \
             --content $content \
             --signing-cert ${KMS_MEMBER_CERT_PATH} \
-            $extra_args \
-            | curl -X POST -s \
-                -H "Authorization: Bearer $bearer_token" \
-                -H "Content-Type: application/json" \
-                "${AKV_URL}/keys/${AKV_KEY_NAME}/sign?api-version=7.2" \
-                -d @- > $signature
+            $extra_args)
+        curl -X POST -s \
+            -H "Authorization: Bearer $bearer_token" \
+            -H "Content-Type: application/json" \
+            "${AKV_URL}/keys/${AKV_KEY_NAME}/sign?api-version=7.2" \
+            -d $payload > $signature
         ccf_cose_sign1_finish \
             --ccf-gov-msg-type $msg_type \
             --ccf-gov-msg-created_at $creation_time \
