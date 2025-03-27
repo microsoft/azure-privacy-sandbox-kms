41e9a3cdc320f8204830a0547dbcd2ad99281193
Dominic Ayre <dominicayre@microsoft.com>
2025-03-27 18:49:11 +0000

    Start refactoring standalone KMS image

--- docker/run_standalone_kms.sh
+++ docker/run_standalone_kms.sh
@@ -3,32 +3,25 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+jwt_issuer_fetch() {
+    curl -X POST "$(cat /kms/workspace/jwt_issuer_address)/token" \
+        | jq -r '.access_token'
+}
+export JWT_ISSUER_WORKSPACE=/kms/workspace
+
 mkdir -p workspace/proposals
+declare -f jwt_issuer_fetch > $JWT_ISSUER_WORKSPACE/fetch.sh
 
 (cd test/utils/jwt && KMS_WORKSPACE=/kms/workspace nohup npm run start > nohup.out 2>&1 &)
 ./scripts/wait_idp_ready.sh
-
-JWK=$(npx pem-jwk "./workspace/private.pem" | \
-      jq --arg cert "$(cat ./workspace/cert.pem)" '{kty, n, e} + {x5c: [$cert]} + {kid: "Demo IDP kid"}')
-
-cat <<EOF | jq > ./workspace/proposals/set_jwt_issuer.json
-{
-  "issuer": "http://Demo-jwt-issuer",
-  "jwks": {
-    "keys": [
-      $JWK
-    ]
-  }
-}
-EOF
 
 /opt/ccf_${CCF_PLATFORM}/bin/sandbox.sh \
   --js-app-bundle ./dist/ \
   --initial-member-count 3 \
   --initial-user-count 1 \
-  --jwt-issuer workspace/proposals/set_jwt_issuer.json \
   -v --http2 "$@" &
 
+export WORKSPACE=/kms/workspace
 export KMS_URL=${KMS_URL:-https://127.0.0.1:8000}
 export KMS_SERVICE_CERT_PATH=./workspace/sandbox_common/service_cert.pem
 export KMS_MEMBER_CERT_PATH=./workspace/sandbox_common/member0_cert.pem
@@ -36,6 +29,15 @@ export KMS_MEMBER_PRIVK_PATH=./workspace/sandbox_common/member0_privk.pem
 
 ./scripts/kms_wait.sh
 
-make setup
+source .venv_ccf_sandbox/bin/activate
 
-tail -f /kms/workspace/sandbox_0/out
+./scripts/kms/release_policy_set.sh governance/proposals/set_key_release_policy_add.json
+
+./scripts/kms/jwt_issuer_trust.sh
+
+make propose-jwt-ms-validation-policy
+
+./scripts/kms/endpoints/refresh.sh
+
+# tail -f /kms/workspace/sandbox_0/out
+sleep infinity
