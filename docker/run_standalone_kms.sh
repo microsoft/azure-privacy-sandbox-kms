a4b935fd3771ea4de5c32b47068de734bc7102ac
Dominic Ayre <dominicayre@microsoft.com>
2025-04-03 13:13:43 +0100

    Refactor the standalone image to remove hardcoded proposals (#448)
    * Make jwt issuer trusting more flexible
    
    * .
    
    * .
    
    * Allow trusting v1 MI
    
    * .
    
    * Fix typo
    
    * Fix typo
    
    * Add some high level documentation for jwt_issuer_trust.sh
    
    * Fix --private-key-path arg
    
    * Start refactoring standalone KMS image
    
    * Move app setting outside of initial container start
    
    * Prebuild python venv to speed up start time
    
    * Don't rebuild app bundle
    
    * Reorg the standalone image runner code
    
    * Fix
    
    * Fix fetching time
    
    * Revert "Prebuild python venv to speed up start time"
    
    This reverts commit 9f3d0196827fae70e90e0bb02d85c7ddfad254b3.
    
    * Trust MI using new script
    
    * Don't rely on az cli
    
    * Give AZ cli to container
    
    * Use explicit args for jwt trusting
    
    * .
    
    * Pass args to run_ccf_network
    
    * Add a retry loop in js_app_set.sh
    
    * .
    
    * .
    
    * Log into az cli if needed
    
    * revert
    
    * add app code later
    
    tmp
    
    * Add large sleep
    
    * Don't rely on default arg for jwt issuer trust
    
    * Propose managed identity
    
    * Add az login step
    
    * Fix missing field in validation policy
    
    * Don't run JWT issuer in standalone image
    
    * Remove field which isn't present in MI tokens
    
    * .
    
    * Specify user refers to aad
    
    * Speed up start time
    
    * Fix makefile commands

--- docker/run_standalone_kms.sh
+++ docker/run_standalone_kms.sh
@@ -3,39 +3,51 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-mkdir -p workspace/proposals
-
-(cd test/utils/jwt && KMS_WORKSPACE=/kms/workspace nohup npm run start > nohup.out 2>&1 &)
-./scripts/wait_idp_ready.sh
-
-JWK=$(npx pem-jwk "./workspace/private.pem" | \
-      jq --arg cert "$(cat ./workspace/cert.pem)" '{kty, n, e} + {x5c: [$cert]} + {kid: "Demo IDP kid"}')
-
-cat <<EOF | jq > ./workspace/proposals/set_jwt_issuer.json
-{
-  "issuer": "http://Demo-jwt-issuer",
-  "jwks": {
-    "keys": [
-      $JWK
-    ]
-  }
-}
-EOF
-
-/opt/ccf_${CCF_PLATFORM}/bin/sandbox.sh \
-  --js-app-bundle ./dist/ \
-  --initial-member-count 3 \
-  --initial-user-count 1 \
-  --jwt-issuer workspace/proposals/set_jwt_issuer.json \
-  -v --http2 "$@" &
-
+export WORKSPACE=/kms/workspace
+export JWT_ISSUER_WORKSPACE=/kms/workspace
 export KMS_URL=${KMS_URL:-https://127.0.0.1:8000}
 export KMS_SERVICE_CERT_PATH=./workspace/sandbox_common/service_cert.pem
 export KMS_MEMBER_CERT_PATH=./workspace/sandbox_common/member0_cert.pem
 export KMS_MEMBER_PRIVK_PATH=./workspace/sandbox_common/member0_privk.pem
 
-./scripts/kms_wait.sh
+mkdir -p $WORKSPACE/proposals
 
-make setup
+if ! az account show > /dev/null 2>&1; then
+  echo "No Azure CLI login detected. Logging in as a managed identity..."
+  az login --identity
+fi
 
-tail -f /kms/workspace/sandbox_0/out
+/opt/ccf_${CCF_PLATFORM}/bin/sandbox.sh \
+  --initial-member-count 3 \
+  --initial-user-count 1 \
+  -v --http2 "$@" &
+
+# Wait for the CCF network to start
+until curl -k -f -s $KMS_URL/node/state && \
+  test -f workspace/sandbox_common/user0_cert.pem; do
+  sleep 1
+done
+
+source .venv_ccf_sandbox/bin/activate
+
+./scripts/kms/js_app_set.sh
+
+./scripts/kms/release_policy_set.sh governance/proposals/set_key_release_policy_add.json
+
+./scripts/kms/jwt_issuer_trust.sh \
+  --private-key-path "$JWT_ISSUER_WORKSPACE/private.pem" \
+  --token "`
+    curl -X POST "$(cat $JWT_ISSUER_WORKSPACE/jwt_issuer_address)/token" \
+      | jq -r '.access_token' \
+  `"
+
+./scripts/kms/jwt_issuer_trust.sh --managed-identity-v1 "` \
+  az identity show --query id -o tsv \
+    --resource-group privacy-sandbox-dev \
+    --name privacysandbox \
+`"
+
+./scripts/kms/endpoints/refresh.sh
+
+sleep infinity
+# tail -f /kms/workspace/sandbox_0/out
