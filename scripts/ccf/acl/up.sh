cd79e6f2a68eb4a1961d890a701c9f869cf97a04
Dominic Ayre <dominicayre@microsoft.com>
2025-04-23 20:54:11 +0000

    .

--- scripts/ccf/acl/up.sh
+++ scripts/ccf/acl/up.sh
@@ -72,7 +72,7 @@ acl-up() {
             --location "AustraliaEast" \
             --ledger-type "Public" \
             --aad-based-security-principals "[{ledger-role-name:Administrator,principal-id:$(az account show | jq -r '.id')}]" \
-            --cert-based-security-principals "[{ledger-role-name:Administrator,cert:$(cat $KMS_MEMBER_CERT_PAH),{ledger-role-name:Reader,cert:$(cat $KMS_USER_CERT_PATH)}]"
+            --cert-based-security-principals "[{ledger-role-name:Administrator,cert:'$(cat $KMS_MEMBER_CERT_PATH)',{ledger-role-name:Reader,cert:'$(cat $KMS_USER_CERT_PATH)'}]"
     else
         echo "Ledger already exists, skipping deployment."
     fi
